bc018951f82792af447b57e6d8dba337cdfba277
Vini Speck <vinicius.speck@catolicasc.org.br>
2022-09-02 02:08:37 -0300

    criacao atv 4

--- atividade 4/main.py
+++ atividade 4/main.py
@@ -1,39 +1,35 @@
-from statistics import median
 import pandas as pd
 
 df = pd.read_csv('tiktok/TikTok_songs_2019.csv', delimiter=',')
-
-arr=[41,64,32,876,34,76,987,5432]
-n= len(arr)
+arr=df['danceability'].to_numpy()
+n = len(arr)
 
 def heapify(arr, n, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1  # left = 2*i + 1
-    r = 2 * i + 2  # right = 2*i + 2
- 
- # See if left child of root exists and is
- # greater than root
- 
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2 
+    
     if l < n and arr[i] < arr[l]:
         largest = l
- 
- # See if right child of root exists and is
- # greater than root
  
     if r < n and arr[largest] < arr[r]:
         largest = r
  
- # Change root, if needed
- 
     if largest != i:
-        (arr[i], arr[largest]) = (arr[largest], arr[i])  # swap
+        (arr[i], arr[largest]) = (arr[largest], arr[i])
  
-  # Heapify the root.
- 
-    heapify(arr, n, largest)
+        heapify(arr, n, largest)
  
  
-# The main function to sort an array of given size
+def heapSort(arr, n):
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
 
-print(heapify(arr,n,0))
+    for i in range(n - 1, 0, -1):
+        (arr[i], arr[0]) = (arr[0], arr[i])
+        heapify(arr, i, 0)
 
+heapSort(arr, n)
+
+for i in range(n):
+    print(arr[i])
